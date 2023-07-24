d6b0537e786659238dd35de85238b3a0e311914b
Hyeonna Cho <hyonna01@gmail.com>
2023-07-25 08:35:52 +0900

    db 연동

--- config/database.go
+++ config/database.go
@@ -1,3 +1,34 @@
 package config
 
-func DBConnection() {}
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	host     = "lineworldap.iptime.org"
+	port     = 33306
+	user     = "apcs_dev"
+	password = "apcs@123"
+	dbName   = "apcs_dev"
+)
+
+func DBConnection() *sql.DB {
+	sqlInfo := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", user, password, host, port, dbName)
+
+	db, err := sql.Open("mysql", sqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Info().Msg("Connected to database!!")
+
+	return db
+}
