301726efec9f928ceef59549bd0460a656922aaa
Hyeonna Cho <hyonna01@gmail.com>
2023-07-25 10:37:10 +0900

    delivery service 작성

--- service/delivery_service.go
+++ service/delivery_service.go
@@ -1,3 +1,26 @@
 package service
 
-type DeliveryServie struct{}
+import (
+	"APCS/config"
+	"APCS/data/response"
+	"APCS/repository"
+)
+
+type DeliveryService struct {
+	Repository *repository.DeliveryRepository
+}
+
+func (d *DeliveryService) InitService() error {
+	db := config.DBConnection()
+
+	d.Repository = &repository.DeliveryRepository{}
+	d.Repository.AssignDB(db)
+
+	return nil
+}
+
+func (d *DeliveryService) CheckDeliveryMatch(deliveryId int) (*[]response.DeliveryReadResponse, error) {
+	resp, err := d.Repository.SelectDeliveryByDeliveryId(deliveryId)
+
+	return resp, err
+}
