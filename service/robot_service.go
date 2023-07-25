91ecd4d9f9989ada0eb2b4c794a0828b7f8c35ca
Hyeonna Cho <hyonna01@gmail.com>
2023-07-25 11:13:33 +0900

    robot service 작성

--- service/robot_service.go
+++ service/robot_service.go
@@ -1,1 +1,20 @@
 package service
+
+type RobotService struct{}
+
+func ScanTrayQrcode() {
+	// 트레이 큐알코드 스캔하라는 요청
+	// 파라미터 - lane, floor, robot_id
+	// 반환값 - qrcode
+}
+
+func MoveTray() {
+	// 트레이를 이동하라는 요청
+	// 파라미터 - (lane, floor), (lane, floor), robot_id
+	// 트레이 db update
+}
+
+func MoveRobot() {
+	// 로봇을 이동하라는 요청
+	// 파라미터 - (lane, floor), (lane, floor), robot_id
+}
