b651f8fdce38886482e17c3d35ea7ac9b4eaab60
Hyeonna Cho <hyonna01@gmail.com>
2023-07-26 09:31:46 +0900

    수납 - 테이블에 빈트레이 유무 감지

--- internal/input_item.go
+++ internal/input_item.go
@@ -28,13 +28,12 @@ func (s *InputItem) StartStorage(req request.DeliveryCreateRequest) {
 	s.SlotService.InitService()
 	s.OwnerService.InitService()
 
-	// 물품기사 일치여부 확인
+	// 1. 물품기사 일치여부 확인
 	s.DeliveryService.CheckDeliveryMatch(req)
 
-	// 테이블에 빈 트레이 유무 감지
-	s.DeliveryBoxService.SenseTableForEmptyTray()
-	b := true
-	if b {
+	// 2. 테이블에 빈 트레이 유무 감지
+	result := s.DeliveryBoxService.SenseTableForEmptyTray()
+	if result {
 		// 있으면 다음 동작
 	} else {
 		resp, _ := s.SlotService.FindSlotListForEmptyTray()
@@ -43,37 +42,30 @@ func (s *InputItem) StartStorage(req request.DeliveryCreateRequest) {
 		s.DeliveryBoxService.SetUpDoor("뒷문", "열림") // 뒷문 열림
 		service.MoveTray()                         // 슬롯에서 테이블로
 		s.DeliveryBoxService.SetUpDoor("뒷문", "닫힘") // 뒷문 닫힘
+		// 슬롯에 빈트레이가 없으면 셀프 트레일 수납
 
 	}
 
-	/* 빈트레이가 있는 슬롯리스트 조회
-				빈 트레이 있음
-					빈 트레이 선정
-					문설정(뒷문, 열림)
-					이동(트레이, 슬롯 -> 테이블)
-					문설정(뒷문, 닫힘)
-				빈 트레이 없음(보류)
-					문설정(앞문, 열림)
-					(물품 기사가 셀프로 트레일 수납)보류
-	문설정(앞문, 열림)
-	물품 정보 입력(바코드, 수기 등)
-		물품 정보 생성
-	수납 완료 버튼
-	문설정(앞문, 닫힘)
-	물품 정보(높이, 무게, 돌출) 감지
-		수납 불가
-			알림(문제점)
-			문설정(앞문, 열림)
-			재수납 버튼 클릭
-				문설정(앞문, 닫힘)
-				물품 정보(높이, 무게, 돌출) 감지
-			수납 취소 버튼 클릭
-				수납 중단
-	수납 가능한 슬롯 조회
-	최적의 슬롯 선정
-	문설정(뒷문, 열림)
-	이동(트레이, 테이블 -> 슬롯)
-	문설정(뒷문, 닫힘)
-	알림(수납완료) */
+	/*
+		문설정(앞문, 열림)
+		물품 정보 입력(바코드, 수기 등)
+			물품 정보 생성
+		수납 완료 버튼
+		문설정(앞문, 닫힘)
+		물품 정보(높이, 무게, 돌출) 감지
+			수납 불가
+				알림(문제점)
+				문설정(앞문, 열림)
+				재수납 버튼 클릭
+					문설정(앞문, 닫힘)
+					물품 정보(높이, 무게, 돌출) 감지
+				수납 취소 버튼 클릭
+					수납 중단
+		수납 가능한 슬롯 조회
+		최적의 슬롯 선정
+		문설정(뒷문, 열림)
+		이동(트레이, 테이블 -> 슬롯)
+		문설정(뒷문, 닫힘)
+		알림(수납완료) */
 
 }
--- service/delivery_box_service.go
+++ service/delivery_box_service.go
@@ -2,36 +2,40 @@ package service
 
 import "fmt"
 
-type DeliveryBoxService struct {
-	SlotService
-}
+type DeliveryBoxService struct{}
 
+// 문을 제어하는 요청
 func (d *DeliveryBoxService) SetUpDoor(gate, operate string) {
-	// 문을 제어하는 요청
 	// 파라미터 - frontgate/backgate, open/close
 	fmt.Println(gate, operate)
 
 }
 
-func (d *DeliveryBoxService) SenseTableForTrayWithItem() {
-	// 테이블에 물품있는 트레이 유무
+// 테이블에 물품있는 트레이 유무
+func (d *DeliveryBoxService) SenseTableForTrayWithItem() bool {
+	a := true
+	return a
 }
 
-func (d *DeliveryBoxService) SenseTableForItem() {
-	// 테이블에 물품 유무
+// 테이블에 물품 유무
+func (d *DeliveryBoxService) SenseTableForItem() bool {
+	a := true
+	return a
 }
 
-func (d *DeliveryBoxService) SenseTableForEmptyTray() {
-	// 테이블에 빈 트레이 유무 감지
+// 테이블에 빈 트레이 유무 감지
+func (d *DeliveryBoxService) SenseTableForEmptyTray() bool {
 	// 센서 동작
-	// 반환값 - true/false
+	a := true
+	return a
 
 }
+
+// 물품정보(높이, 무게) 감지
 func (d *DeliveryBoxService) SenseItemInfo() {
-	// 물품정보(높이, 무게) 감지
 	// 반환값 - 높이, 무게
 }
 
+// 미이동 감지
 func (d *DeliveryBoxService) SenseNotMoveForxm() {
-	// 미이동 감지
 }
