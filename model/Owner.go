b2542daebe41b363cc3a5407b5b62cb67431aa26
choieungyu <choieungyu.dev@gmail.com>
2023-08-21 16:55:21 +0900

    Fix: Add Owner repository and test

--- model/Owner.go
+++ model/Owner.go
@@ -2,33 +2,40 @@ package model
 
 import log "github.com/sirupsen/logrus"
 
-type Owner struct {
-	Id   int    `json:"owner_id"`
-	Name string `json:"owner_name"`
+type OwnerInfo struct {
+	OwnerName string
+	PhoneNum  string
+	Address   string
 }
 
-func FindAllOwners() ([]Owner, error) {
-	// DB 테스트 접근
-	rows, err := db.Query("SELECT owner_id, owner_name FROM TN_INF_OWNER")
+type Owner struct {
+	OwnerId   int64
+	OwnerName string
+	PhoneNum  string
+	Address   string
+}
+
+func SelectOwnerByOwnerInfo(info OwnerInfo) (Owner, error) {
+	query :=
+		`SELECT 
+    			owner_id, 
+    			owner_name, 
+    			phone_num, 
+    			address
+		FROM TN_INF_OWNER
+		WHERE owner_name = ? 
+			AND phone_num = ? 
+		  	AND address = ?
+		`
+
+	var owner Owner
+
+	row := db.QueryRow(query, info.OwnerName, info.PhoneNum, info.Address)
+	err := row.Scan(&owner.OwnerId, &owner.OwnerName, &owner.PhoneNum, &owner.Address)
 	if err != nil {
 		log.Error(err)
+		return Owner{}, err
 	}
 
-	var owners []Owner
-
-	for rows.Next() {
-		var owner Owner
-
-		if err = rows.Scan(&owner.Id, &owner.Name); err != nil {
-			log.Error(err)
-		}
-
-		owners = append(owners, owner)
-	}
-
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return owners, nil
+	return owner, nil
 }
